a4c8a5ba8f6a12f98f3e0482136c2f25da973c1f
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2015-10-07 11:23:00 -0400

    fixed long questions

--- AnalysingQuora/main.py
+++ AnalysingQuora/main.py
@@ -2,32 +2,32 @@
 This script will contain the main part of what's happening.
 """
 import json
-dataset = open('quoradataset.txt')
+dataset = open('sample_quora.txt')
 long_questions = open('long_q.txt', 'w')
 
 def select_long_questions(dataset):
     for s in dataset:
-        jobj = json.loads(s.strip())
+        d = eval(s)
 
-        if not (jobj['qbody'] and jobj['answers']):
+        if not (d['qbody'] and d['answers']):
             continue
-        long_questions.write('%s\n' % str(jobj))
+        long_questions.write('%s\n' % str(d))
 
 
 def main():
     log = open('logging.txt', 'w')
-    questions = select_long_questions()
-    for quest in questions:
-        log.write('***QUESTION***\n%s\n' + quest)
-        queries = construct_queries(quest)
-        log.write('***QUERIES***\n')
-        i = 1
-        for q in queries:
-            log.write('%d. %s\n' % (i, q))
-            i += 1
-            docs = askBing(q)
-            for d in docs:
-                log.write('%s\n' % d['href'])
+    questions = select_long_questions(dataset)
+    # for quest in questions:
+    #     log.write('***QUESTION***\n%s\n' + quest)
+    #     queries = construct_queries(quest)
+    #     log.write('***QUERIES***\n')
+    #     i = 1
+    #     for q in queries:
+    #         log.write('%d. %s\n' % (i, q))
+    #         i += 1
+    #         docs = askBing(q)
+    #         for d in docs:
+    #             log.write('%s\n' % d['href'])
 
 if __name__ == '__main__':
     main()
